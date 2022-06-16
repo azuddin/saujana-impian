f115479eca3f13ae31d6f89948637398a26cd9b8
Ahmad Azuddin <exectraquasar@gmail.com>
2022-06-16 16:13:15 +0800

    add site plan

---
+++ pages/site-plan.tsx
@@ -0,0 +1,57 @@
+import { Box, Text } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Layout } from "../components/Layout";
+
+const SitePlanPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Project - Saujana Impian @ Seremban 2 Height</title>
+        <meta name="description" content="Saujana Impian @ Seremban 2 Height" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <div className="flex">
+          <div className="w-2/3">
+            <img src="/assets/images/page-3/SitePlanImg.png" />
+          </div>
+          <div className="flex flex-grow flex-col items-center pt-20 space-y-14">
+            <div className="h-20">
+              <img
+                src="/assets/images/page-3/SitePlan@2x.png"
+                className="h-full"
+              />
+            </div>
+            <div className="flex items-center space-x-2">
+              <Text fontSize="xl" className="leading-none">
+                TOTAL
+                <br />
+                UNITS:
+              </Text>
+              <Text fontSize="5xl" className="leading-none">
+                42
+              </Text>
+            </div>
+            <div className="flex flex-col flex-start space-y-2">
+              <div className="flex items-center space-x-4">
+                <Box height={6} width={6} backgroundColor="#E1BA8B"></Box>
+                <Text fontSize="xl" fontWeight="semibold">
+                  2 STOREY SEMI-D
+                </Text>
+              </div>
+              <div className="flex items-center space-x-4">
+                <Box height={6} width={6} backgroundColor="#B099C8"></Box>
+                <Text fontSize="xl" fontWeight="semibold">
+                  2 STOREY BUNGALOW
+                </Text>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default SitePlanPage;
