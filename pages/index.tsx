a7d50fbebb84cd883a869856e0a8b34c5ea5e544
Ahmad Azuddin <exectraquasar@gmail.com>
2022-06-25 06:09:56 +0800

    slider nak besar lagi

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Layout } from "../components/Layout";
 import { useMediaQuery } from "@chakra-ui/react";
+import { Navbar } from "../components/Navbar";
 
 const Home: NextPage = () => {
   const [isDesktop] = useMediaQuery("(min-width: 1280px)");
@@ -17,47 +18,46 @@ const Home: NextPage = () => {
         <meta name="description" content="Saujana Impian @ Seremban 2 Height" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout>
-        <Swiper
-          className="mt-24 xl:mt-0"
-          pagination={true}
-          modules={[Pagination]}
-          slidesPerView={1}
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
-        >
-          <SwiperSlide>
-            <img
-              className="mx-auto mb-14"
-              src={
-                isDesktop
-                  ? "/assets/images/banner/slider-1.jpeg"
-                  : "/assets/images/Slider-1@2x.jpg"
-              }
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="mx-auto mb-14"
-              src={
-                isDesktop
-                  ? "/assets/images/banner/slider-2.jpeg"
-                  : "/assets/images/Slider-2@2x.jpg"
-              }
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="mx-auto mb-14"
-              src={
-                isDesktop
-                  ? "/assets/images/banner/slider-3.jpeg"
-                  : "/assets/images/Slider-3@2x.jpg"
-              }
-            />
-          </SwiperSlide>
-        </Swiper>
-      </Layout>
+      <Navbar></Navbar>
+      <Swiper
+        className="mt-24 xl:mt-0"
+        pagination={true}
+        modules={[Pagination]}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <img
+            className="mx-auto mb-14"
+            src={
+              isDesktop
+                ? "/assets/images/banner/slider-1.jpeg"
+                : "/assets/images/Slider-1@2x.jpg"
+            }
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="mx-auto mb-14"
+            src={
+              isDesktop
+                ? "/assets/images/banner/slider-2.jpeg"
+                : "/assets/images/Slider-2@2x.jpg"
+            }
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="mx-auto mb-14"
+            src={
+              isDesktop
+                ? "/assets/images/banner/slider-3.jpeg"
+                : "/assets/images/Slider-3@2x.jpg"
+            }
+          />
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 };
